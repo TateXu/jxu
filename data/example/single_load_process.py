b17b153202617f7c1154e4dcff644e477ba2af31
Jiachen Xu <jiachen.xu@ieee.org>
2020-03-31 11:58:14 +0200

    Finalize joint time-freq analysis and joint topomap

--- data/example/single_load_process.py
+++ data/example/single_load_process.py
@@ -100,9 +100,46 @@ def trigger_detector(raw, event_dict=event_dict,
               str(nr_start) + '/' + str(nr_end))
 
 
+def plot_joint_psd(raw_file, freq=[(0.0, 70.0)], nfft=1000, save=False,
+                   tmin=None, tmax=None,
+                   fig_name='test.pdf', fig_unit_height=3, picks=None,
+                   fig_unit_width=3, fig_height=None, fig_width=None):
+
+    nr_band = len(freq)
+    if fig_height is None:
+        fig_height = 2 * fig_unit_height
+
+    if fig_width is None:
+        fig_width = nr_band * fig_unit_width
+
+    fig = plt.figure(figsize=(fig_width, fig_height))
+    axes = []
+    gs0 = gs.GridSpec(nrows=2, ncols=nr_band)
+    for ax_col in range(nr_band):
+        axes.append(fig.add_subplot(gs0[0, ax_col]))
+    axes.append(gs0[1, :])
+    axes = np.asarray(axes)
+
+    ax3 = plt.subplot(212)
+
+    raw_file.plot_psd_topomap(axes=axes[:-1], tmin=tmin, tmax=tmax,
+                              bands=freq, show=False)
+
+    raw_file.plot_psd(average=False, ax=ax3, spatial_colors=True,
+                      picks=picks, estimate='amplitude', tmin=tmin, tmax=tmax,
+                      fmin=freq[0][0], fmax=freq[-2][1], show=False)
+
+
+    if save:
+        fig.savefig(fig_name)
+        print('Figure saved!')
+
+
+
 def plot_joint_t_freq(raw_file, channel='C3', n_perseg=1000, nfft=1000,
-                      n_overlap=None, colorbar=False, save=False,
-                      tmin=None, tmax=None, fmin=0.0, fmax=70.0):
+                      n_overlap=None, colorbar=True, save=False,
+                      tmin=None, tmax=None, t_step=None, fmin=0.0, fmax=70.0,
+                      num_t_step=None, fig_name='test.pdf'):
 
     if not isinstance(raw_file, list):
         raw_file = [raw_file]
@@ -115,6 +152,11 @@ def plot_joint_t_freq(raw_file, channel='C3', n_perseg=1000, nfft=1000,
         tmin = raw_file[0].tmin
     if tmax is None:
         tmax = raw_file[0].tmax
+    if num_t_step is None:
+        num_t_step = 11
+    if t_step is None:
+        t_step = (tmax - tmin) / num_t_step
+
 
     fs = raw_file[0].info['sfreq']
     down_fs = int(fs / 5)
@@ -122,31 +164,42 @@ def plot_joint_t_freq(raw_file, channel='C3', n_perseg=1000, nfft=1000,
     t_resolution = n_overlap / fs
     f_resolution = fs / nfft
 
-    tmax = 190
     min_t_samp = int(tmin / t_resolution)
     max_t_samp = int(tmax / t_resolution)
     min_f_samp = int(fmin / f_resolution)
     max_f_samp = int(fmax / f_resolution)
 
     nr_raw = len(raw_file) * len(channel)
-    xlabel, ylabel, title, rotation, axis_lim = [], [], [], [], []
-    xlabel += [['ax', 'bx'], ['cx', 'dx']] * nr_raw
-    ylabel += [['ax', 'bx'], ['cx', 'dx']] * nr_raw
-    title += [['ax', 'bx'], ['cx', 'dx']] * nr_raw
+    xlabel, ylabel, title, scale, rotation, axis_lim = [], [], [], [], [], []
+    xlabel += [['', 'Time/s'], ['', 'Time/s']] * nr_raw
+    ylabel += [['Amplitude', 'Frequency/Hz'], ['', 'Amplitude/uV']] * nr_raw
+    title += [['Spectrum', 'Short Time Fourier Transform'], ['Electrodes Location', 'Amplitude of EEG signal']] * nr_raw
+
+    scale += [[[4.3, 2], [1, 1]], [[1, 1], [1, 1]]] * nr_raw
     rotation += [[90, 0], [0, 0]] * nr_raw
-    axis_lim += [[(0, 70, 0, 90), 0], [0, 0]] * nr_raw
+    axis_lim += [[(0, 70, 0, 70), 0], [0, 0]] * nr_raw
     row_ratios = []
     row_ratios += [2, 1] * nr_raw
 
-    fig, axes = fig_init(nr_row=2 * nr_raw, nr_col=2,
-                         row_ratios=row_ratios, col_ratios=[1, 3],
-                         fig_unit_height=6, fig_unit_width=6,
-                         xlabel=np.asarray(xlabel),
-                         ylabel=np.asarray(ylabel),
-                         title=np.asarray(title),
-                         rotation=np.asarray(rotation),
-                         axis_lim=np.asarray(axis_lim))
-    pdb.set_trace()
+    xticks, xticklabels = [], []
+    xticks += [[None, np.linspace(tmin, tmax, num_t_step)],
+               [None, np.linspace(tmin, tmax, num_t_step)]] * nr_raw
+
+    xticklabels += [[None, [str(i) for i in np.linspace(tmin, tmax, num_t_step)]],
+                    [None, [str(i) for i in np.linspace(tmin, tmax, num_t_step)]]] * nr_raw
+
+    xticks = np.asarray(xticks)
+    xticklabels = np.asarray(xticklabels)
+    fig, axes, gs = fig_init(nr_row=2 * nr_raw, nr_col=2,
+                             row_ratios=row_ratios, col_ratios=[1, 3],
+                             fig_unit_height=3, fig_unit_width=3,
+                             xlabel=np.asarray(xlabel),
+                             ylabel=np.asarray(ylabel),
+                             title=np.asarray(title),
+                             scale=np.asarray(scale),
+                             rotation=np.asarray(rotation),
+                             axis_lim=np.asarray(axis_lim))
+
     for ind_raw, (sig_raw, sig_chn) in enumerate(product(raw_file, channel)):
         print(ind_raw)
         print(sig_raw)
@@ -165,24 +218,33 @@ def plot_joint_t_freq(raw_file, channel='C3', n_perseg=1000, nfft=1000,
             vmin=0, vmax=6)
         if colorbar:
             plt.colorbar(mappable=stft_fig, ax=axes[ind_raw * 2, 1],
-                         use_gridspec=True,
-                         orientation='vertical')
-
+                         orientation='vertical',
+                         use_gridspec=True)
         sig_raw.plot_psd(average=False, ax=axes[ind_raw * 2, 0],
                          spatial_colors=True, picks=picks,
+                         estimate='amplitude',
                          tmin=tmin, tmax=tmax,
                          fmin=fmin, fmax=fmax, show=False)
         sig_raw.info['bads'] = picks
         sig_raw.plot_sensors(show=False, show_names=False,kind='select',
                              axes=axes[ind_raw * 2 + 1, 0],
-                             title='')
-        raw_ts = sig_raw.resample(sfreq=down_fs).get_data(picks=picks) * 1e6
-        axes[ind_raw * 2 + 1, 1].plot(
-            raw_ts[0][0][int(tmin * down_fs): int(tmax * down_fs)])
+                             title=sig_chn)
+        raw_ts = sig_raw.copy().resample(sfreq=down_fs).get_data(picks=picks) * 1e6
+
+        axes[ind_raw * 2, 0].set_xlim([fmin, fmax])
+        axes[ind_raw * 2 + 1, 1].plot(np.arange(tmin * down_fs, tmax * down_fs + 1, 1),
+            raw_ts[0][0][int(tmin * down_fs): int(tmax * down_fs) + 1])
+
+        axes[ind_raw * 2 + 1, 0].set_title('Channel: ' + sig_chn)
+
+        axes[ind_raw * 2 + 1, 1].set_xlim([tmin * down_fs, tmax * down_fs + 1])
+        axes[ind_raw * 2 + 1, 1].set_xticks(down_fs * xticks[ind_raw * 2 + 1, 1])
+        axes[ind_raw * 2 + 1, 1].set_xticklabels(xticklabels[ind_raw * 2 + 1, 1])
+
     fig.tight_layout()
-    fig.subplots_adjust(wspace=-0.05)
+    fig.subplots_adjust(wspace=0.2, hspace=0.2)
     if save:
-        fig.savefig('test.pdf')
+        fig.savefig(fig_name)
         print('Figure saved!')
 """
 for bandpass in filters:
@@ -192,7 +254,7 @@ for bandpass in filters:
                               picks=picks, verbose=False, fir_design='firwin')
 
 """
-load = False  # True
+load = True  # True
 
 path = '/Users/xujiachen/File/Data/NIBS/Stage_one/ZWS/ZWS_SESSION_1/'
 if load:
@@ -204,22 +266,36 @@ if load:
 
     fmin, fmax = filters[0]
     # filter data
-    raw_f = raw.copy().filter(fmin, fmax, method='fir', verbose=False, fir_design='firwin')
 
-    print('filtered!')
-    data_run_0_1 = raw_f[:, : 2446629]
-    data_run_2 = raw_f[:, 2578697:3503294]
-    data_run_3 = raw_f[:, 3552652:]
+    raw.set_channel_types({'Audio': 'stim', 'tACS': 'stim'})
+    pdb.set_trace()
+    print('Start filtering!')
+    raw_f = raw.copy().filter(fmin, fmax, method='fir', verbose=False, fir_design='firwin')
+    raw_f.notch_filter(freqs=np.arange(50, 249, 50), picks='eeg')
+
+    raw_f.set_channel_types({'EOG151': 'eog', 'EOG152': 'eog'})
+    print('Removing common average!')
+
+    raw_ca = raw_f.copy().set_eeg_reference(ref_channels='average')
+
+    # ori_data_eog = raw_f.get_data()[-2:,:]
+    # ori_data = raw_f.pick(picks='eeg').get_data()
+    # ca_data_eog = raw_ca.get_data()[-2:,:]
+    # ca_data = raw_ca.copy().pick(picks='eeg').get_data()
+
+    data_run_0_1 = raw_ca[:, : 2446629]
+    data_run_2 = raw_ca[:, 2578697:3503294]
+    data_run_3 = raw_ca[:, 3552652:]
 
     ts_run_0_1 = data_run_0_1[1]
     ts_run_2 = data_run_2[1]
     ts_run_3 = data_run_3[1]
 
-    raw_run_0_1 = raw_f.copy().crop(
+    raw_run_0_1 = raw_ca.copy().crop(
         tmin=ts_run_0_1[0], tmax=ts_run_0_1[-1], include_tmax=True)
-    raw_run_2 = raw_f.copy().crop(
+    raw_run_2 = raw_ca.copy().crop(
         tmin=ts_run_2[0], tmax=ts_run_2[-1], include_tmax=True)
-    raw_run_3 = raw_f.copy().crop(
+    raw_run_3 = raw_ca.copy().crop(
         tmin=ts_run_3[0], tmax=ts_run_3[-1], include_tmax=True)
 
     raw_clean = raw_run_0_1.copy()
@@ -240,11 +316,12 @@ if load:
     raw_clean.set_montage(montage)
     raw_clean.rename_channels({'I1': 'O9', 'I2': 'O10'})
     raw_clean.info['bads'] = ['TPP9h', 'P7', 'AFp1']
-    raw_clean.set_channel_types({'Audio': 'stim', 'tACS': 'stim'})
+    # raw_clean.set_channel_types({'Audio': 'stim', 'tACS': 'stim'})
     raw_clean.set_channel_types({'TP7': 'stim', 'TP8': 'stim',
                                  'TTP7h': 'stim', 'TTP8h': 'stim',
-                                 'TPP7h': 'stim', 'TPP8h': 'stim'})
-    raw_clean.set_channel_types({'EOG151': 'eog', 'EOG152': 'eog'})
+                                 'TPP7h': 'stim', 'TPP8h': 'stim',
+                                 'T8': 'stim'})
+    # raw_clean.set_channel_types({'EOG151': 'eog', 'EOG152': 'eog'})
 
     for ind, (key, val) in enumerate(epoch_list.items()):
         epochs = mne.Epochs(raw_clean, events_clean,
@@ -273,26 +350,44 @@ if load:
     with open(path + 'data.pkl', 'wb') as f:
         pickle.dump(X, f)
     print('saved!')
-
+pdb.set_trace()
 with open(path + 'data.pkl', 'rb') as f:
     X = pickle.load(f)
 
-
+"""
 full_chn = X['RS']['open'][0].info['ch_names']
 exclude = ['Audio', 'tACS', 'EOG151', 'EOG152', 'TP7', 'TP8', 'TTP7h', 'TTP8h',
            'TPP7h', 'TPP8h', 'TPP9h', 'P7', 'AFP1']
 
 picks = list(set(full_chn) - set(exclude))
+"""
 
-
-picks = ['C3', 'C4']
+picks = ['Pz', 'Fz']
 
 pdb.set_trace()
-plot_joint_t_freq([X['RS']['open'][0]], channel=picks,
-                  colorbar=False, fmin=0.0, save=True,
-                  tmin=30.0, tmax=35.0)
+"""
+for nr_run in range(4):
+    plot_joint_t_freq([X['RS']['open'][nr_run], X['RS']['close'][nr_run]], channel=picks,
+                      colorbar=False, fmin=0.0, save=True, fmax=70.0,
+                      tmin=30.0, tmax=180.0, fig_name='RS_open_vs_close_Run_' + str(nr_run) + '.pdf')
 
+for state in ['open', 'close']:
+    for nr_run in range(4):
+        plot_joint_psd(
+            X['RS'][state][nr_run],
+            freq=[(1.0, 4.0, 'Delta (1-4)'),
+                  (4.0, 8.0, 'Theta (4-8)'),
+                  (8.0, 14.0, 'Alpha (8-14)'),
+                  (14.0, 20.0, 'Low Beta (14-20)'),
+                  (20.0, 30.0, 'High Beta (20-30)'),
+                  (30.0, 70.0, 'Gamma (30-70))'),
+                  (8.9, 9.1, 'Peak Frequency (9Hz)')],
+            tmin=30.0, tmax=180.0, picks='eeg',
+            fig_name='RS_' + state + '_run_' + str(nr_run) + '.pdf',
+            fig_unit_height=3, save=True,
+            fig_unit_width=3, fig_height=None, fig_width=None)
 
+"""
 
 pdb.set_trace()
 """
@@ -397,3 +492,36 @@ rs_close_end = events[np.where(events[:,2]==35)[0], :]
 
 # fig = mne.viz.plot_events(events, sfreq=raw.info['sfreq'],first_samp=raw.first_samp, event_id=new_event_dict)
 
+
+"""
+if colorbar:
+    xlabel, ylabel, title, scale, rotation, axis_lim = [], [], [], [], [], []
+    xlabel += [['', 'Time/s', ''], ['', 'Time/s', '']] * nr_raw
+    ylabel += [['Amplitude', 'Frequency/Hz', 'Amplitude'], ['', 'Amplitude/uV', '']] * nr_raw
+    title += [['Spectrum', 'Short Time Fourier Transform', 'Cbar'], ['Electrodes Location', 'Amplitude of EEG signal', '']] * nr_raw
+
+    scale += [[[4.3, 2], [1, 1], [0.2, 1]], [[1, 1], [1, 1], [1, 1]]] * nr_raw
+    rotation += [[90, 0, 90], [0, 0, 0]] * nr_raw
+    axis_lim += [[(0, 70, 0, 70), 0, (0, 70, 0, 70)], [0, 0, 0]] * nr_raw
+    row_ratios = []
+    row_ratios += [2, 1] * nr_raw
+
+    xticks, xticklabels = [], []
+    xticks += [[None, np.linspace(tmin, tmax, num_t_step), None],
+               [None, np.linspace(tmin, tmax, num_t_step), None]] * nr_raw
+
+    xticklabels += [[None, [str(i) for i in np.linspace(tmin, tmax, num_t_step)], None],
+                    [None, [str(i) for i in np.linspace(tmin, tmax, num_t_step)], None]] * nr_raw
+
+    xticks = np.asarray(xticks)
+    xticklabels = np.asarray(xticklabels)
+    fig, axes = fig_init(nr_row=2 * nr_raw, nr_col=3,
+                         row_ratios=row_ratios, col_ratios=[1, 3, 1],
+                         fig_unit_height=3, fig_unit_width=3,
+                         xlabel=np.asarray(xlabel),
+                         ylabel=np.asarray(ylabel),
+                         title=np.asarray(title),
+                         scale=np.asarray(scale),
+                         rotation=np.asarray(rotation),
+                         axis_lim=np.asarray(axis_lim))
+"""
