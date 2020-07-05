6fba869e23a2126af6573be135cccf6fb2786890
root <jiachen.xu.94@gmail.com>
2020-07-05 23:09:38 +0200

    add filter function to class NIBSEEG

--- data/preprocess.py
+++ data/preprocess.py
@@ -29,16 +29,18 @@ from .utils import BaseEEG
 
 class NIBSEEG():
 
-    def __init__(self, subject=1, session=0, band=[0.1, 70.0], notch=[50.0],
+    def __init__(self, subject=1, session=0, nr_seg=2, bands=[(0.1, 70.0)],
                  root='/home/jxu/File/Data/NIBS/Stage_one/EEG/Exp/',
-                 reref='CA', filter_setting=[()]):
+                 filter_para=dict(method='iir', iir_params=dict(order=5, ftype='butter')),
+                 reref='CA'):
+
         self.subject = subject
         self.session = session
-        self.band = band
-        self.notch = notch
+        self.nr_seg = nr_seg
+        self.bands = bands
         self.root = root
         self.reref = reref
-        self.filter_setting = filter_setting
+        self.filter_para = filter_para
         self.subject_list = {'test': [],
                              'TES': [10, 0, 40, 4],
                              'NUK': [40, 4, 10, 0],
@@ -54,21 +56,42 @@ class NIBSEEG():
 
     # -------- File opeartion ---------
 
-    def load_raw(self, nr_seg=2):
-        self.raw_path(nr_seg=nr_seg)
+    def raw_load(self):
+        self.raw_path()
         self.raw_data = [vhdr_load(self.root + eeg_file_path) for eeg_file_path in self.eeg_file_path_list]
 
-        if len(self.raw_data) != nr_seg:
+        [single_raw.set_channel_types({'Audio': 'stim', 'tACS': 'stim'}) for single_raw in self.raw_data]
+        if len(self.raw_data) != self.nr_seg:
             raise ValueError("#Seg != #Raw data")
 
         return self
 
-    def raw_path(self, nr_seg=2):
+    def raw_path(self):
 
         self.subj_id = list(self.subject_list.keys())[self.subject]
         self.eeg_folder = '{0}/Session_{1}/'.format(self.subj_id, str(int(self.session)))
-        self.audio_folder = '{0}/Audio/Session_{1}/Exp_data/'.format(self.subj_id, str(int(self.session)).zfill(2))
-        self.eeg_file_path_list = ['{0}{1}_seg_{2}.vhdr'.format(self.eeg_folder, self.subj_id, str(i)) for i in range(nr_seg)]
+        self.audio_folder = '{0}/Audio/Session_{1}/Exp_data/'.format(
+            self.subj_id, str(int(self.session)).zfill(2))
+        self.eeg_file_path_list = [
+            '{0}{1}_seg_{2}.vhdr'.format(
+                self.eeg_folder, self.subj_id, str(i)) for i in range(self.nr_seg)]
+
+        return self
+
+
+    def raw_filter(self):
+        print('------- Start filtering-------')
+        if not hasattr(self, 'raw_data') or self.raw_data is None:
+            self.raw_load()
+
+        self.filtered_data = []
+        for fmin, fmax in self.bands:
+            for raw in self.raw_data:
+                raw_f = raw.copy().filter(l_freq=fmin, h_freq=fmax,
+                                          verbose=False, **self.filter_para)
+                # method='fir', fir_design='firwin'
+                raw_f.notch_filter(freqs=np.arange(50, 99, 50), picks='eeg')
+                self.filtered_data.append(raw_f)
 
         return self
 
