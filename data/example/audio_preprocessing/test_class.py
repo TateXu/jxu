0fafdca1694de05f9ca1a3fccf24eb529537ad5b
root <jiachen.xu.94@gmail.com>
2020-10-16 13:33:06 +0200

    Update the comment for the example code for running NIBSAudio class

--- data/example/audio_preprocessing/test_class.py
+++ data/example/audio_preprocessing/test_class.py
@@ -15,24 +15,53 @@ from scipy.stats import f_oneway
 ses_audio = NIBSAudio(subject=10, session=1)
 
 
-# Load stadardised audio files: 44100sps, 16bit, mono channel
+# =============================================================================
+# STEP 1: Load stadardised audio files: 44100sps, 16bit, mono channel
+# =============================================================================
+# For standardising audio file, use audio_std_batch.py
+
+# =============================================================================
+# STEP 2: Process raw audio files into valid audio segments
+# =============================================================================
+
+# -> 1. Filtering: optimal level 5
+# -> 2. Segmentation
+# -> 3. Automatically detect segment based on amplitude: optimal ET - 51.5
+# -> 4. Manually detect bad segments
+# -> 5. Manually adjust onset and transcribe segment into text
+
+# RETURN
+# =============================================================================
+# |    Func    |    File    |    Var    |                Format               |
+# -----------------------------------------------------------------------------
+# |  audio_seg |  audio_folder/Marker/answer.pkl  | self.seg_marker |
+# [#trials] * [#segs, noise_flag, onset, onset+dur, ext_l, ext_r ]   |
+# =============================================================================
+
+# RETURN
+
+# -----------------------------------------------------------------------------
+# |    Func    |    File    |    Var    |                Format               |
+# -----------------------------------------------------------------------------
+# |  valid_seg | audio_folder/Marker/valid_answer.pkl | self.valid_seg_marker |
+# [#trials] * [noise_flag, loc of valid seg audio, onset, duration, text]   |
+# -----------------------------------------------------------------------------
 
 ses_audio.audio_load(audio_type='question')
 try:
-    ses_audio.audio_load(preload=False, std=True)
-    ses_audio.audio_to_seg(opt_noise_level=5, opt_ET=51.5)
+    ses_audio.audio_load(preload=False, std=True) # Step 1
+    ses_audio.audio_to_seg(opt_noise_level=5, opt_ET=51.5) # Step 2: P1-P4
 except FileNotFoundError:
-    print('Std audio file does not exist.')
+    print('Std audio file does not exist. Following code works for single ' +
+          'session. For batch processing the audio files, please run ' +
+          'audio_std_batch.py first')
     ses_audio.audio_load(preload=False, std=False)
     ses_audio.audio_std()
     ses_audio.audio_load(preload=False, std=True)
     ses_audio.audio_to_seg(opt_noise_level=5, opt_ET=51.5)
-# Process answer into segment:
-# - 1. Denoise
-# - 2. Seg
-ses_audio.valid_seg()
 
-ses_audio.audio_load(audio_type='question')
+ses_audio.valid_seg() # Step 2: P5
+
 
 # ses_audio.qa_combine()
 
