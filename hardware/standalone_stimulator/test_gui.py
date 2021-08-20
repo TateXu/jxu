006ea098325f3b21d762087f9d57c275b1fbf480
root <jiachen.xu.94@gmail.com>
2021-08-20 19:40:09 +0200

    add the scpi command

--- hardware/standalone_stimulator/test_gui.py
+++ hardware/standalone_stimulator/test_gui.py
@@ -22,7 +22,7 @@ class MyWindow:
     def __init__(self, window, fontsize=20):
         self.window = window
         self.window_geometry()
-        self.fontStyle = tkFont.Font(family="Lucida Grande", size=fontsize)
+        self.fontStyle = tkFont.Font(family="Times New Roman", size=fontsize) # "Lucida Grande"
         self.button_place()
         self.wave_display()
 
@@ -34,7 +34,7 @@ class MyWindow:
         self.ch2_label = Label(self.window, text='CH2', font=self.fontStyle)
 
         self.freq_label = Label(self.window, text='Frequency (Hz)', font=self.fontStyle)
-        self.phase_label = Label(self.window, text='Phase (°)', font=self.fontStyle)
+        self.phase_label = Label(self.window, text='Phase (Degree)', font=self.fontStyle)
         self.amp_label = Label(self.window, text='Voltage (V)', font=self.fontStyle)
         self.offset_label = Label(self.window, text='Offset (V)', font=self.fontStyle)
         self.output_label = Label(self.window, text='Output', font=self.fontStyle)
@@ -75,10 +75,18 @@ class MyWindow:
     def wave_display(self):
 
         self.fig, self.ax = plt.subplots(2, 1)
-        self.ax[0].set_title('CH0')
-        self.ax[1].set_facecolor('xkcd:black')
-        self.ax[0].set_title('CH2')
+        self.ax[0].set_title('CH1')
         self.ax[0].set_facecolor('xkcd:black')
+        self.ax[0].grid(True)
+        self.ax[0].set_xlabel('Time/sec')
+        self.ax[0].set_ylabel('Voltage/V')
+
+        self.ax[1].set_title('CH2')
+        self.ax[1].set_facecolor('xkcd:black')
+        self.ax[1].grid(True)
+        self.ax[1].set_xlabel('Time/sec')
+        self.ax[1].set_ylabel('Voltage/V')
+
         plt.ion()
         t = np.arange(0.0,3.0,0.01)
         s = np.sin(np.pi*t)
@@ -97,28 +105,61 @@ class MyWindow:
 
         # plot_widget.pack(expand=True, side=LEFT)
         plot_widget.grid(row=0, column=0, rowspan=7)
-        tk.Button(self.window, text="Update CH1", command=update).grid(row=5, column=2,)
-        tk.Button(self.window, text="Update CH2", command=update).grid(row=5, column=3)
-        tk.Button(self.window, text="Refresh", command=self.sin_update).grid(row=7, column=1, columnspan=3)
+        tk.Button(self.window, text="CH1", command=lambda: self.signal_out(chn=1)).grid(row=5, column=2,)
+        tk.Button(self.window, text="CH2", command=lambda: self.signal_out(chn=2)).grid(row=5, column=3)
+        tk.Button(self.window, text="Update Parameters", command=self.sin_update).grid(row=6, column=1, columnspan=3)
         # self.window.mainloop()
+    def signal_out(self, chn):
+        if chn == 1:
+            print('CH1')
+        else:
+            print('CH2')
+        pass
 
 
     def sin_update(self):
 
+        self.freq_val_ch1 = float(self.freq_entry_ch1.get())
+        self.phase_val_ch1 = float(self.phase_entry_ch1.get())
+        self.amp_val_ch1 = float(self.amp_entry_ch1.get())
+        self.offset_val_ch1 = float(self.offset_entry_ch1.get())
+
+        self.freq_val_ch2 = float(self.freq_entry_ch2.get())
+        self.phase_val_ch2 = float(self.phase_entry_ch2.get())
+        self.amp_val_ch2 = float(self.amp_entry_ch2.get())
+        self.offset_val_ch2 = float(self.offset_entry_ch2.get())
+
         t = np.arange(0.0, 1.0, 0.001)
-        ch1_signal = float(self.offset_entry_ch1.get()) + \
-            float(self.amp_entry_ch1.get()) * np.sin(
-                2*np.pi*float(self.freq_entry_ch1.get())*t + \
-                float(self.phase_entry_ch1.get()) / 180 * np.pi)
+        ch1_signal = self.offset_val_ch1 + self.amp_val_ch1 * np.sin(
+            2*np.pi*self.freq_val_ch1*t + self.phase_val_ch1 / 180 * np.pi)
         self.ax[0].clear()
         self.ax[0].plot(t, ch1_signal, color='xkcd:yellow green')
 
-        ch2_signal = float(self.offset_entry_ch2.get()) + \
-            float(self.amp_entry_ch2.get()) * np.sin(
-                2*np.pi*float(self.freq_entry_ch2.get())*t + \
-                float(self.phase_entry_ch2.get()) / 180 * np.pi)
+        ch2_signal = self.offset_val_ch2 + self.amp_val_ch2 * np.sin(
+            2*np.pi*self.freq_val_ch2*t + self.phase_val_ch2 / 180 * np.pi)
         self.ax[1].clear()
         self.ax[1].plot(t, ch2_signal, color='xkcd:sky blue')
+
+        self.ax[0].set_title('CH1')
+        self.ax[0].set_facecolor('xkcd:black')
+        self.ax[0].grid(True)
+        self.ax[0].set_xlabel('Time/sec')
+        self.ax[0].set_ylabel('Voltage/V')
+        self.ax[1].set_title('CH2')
+        self.ax[1].set_facecolor('xkcd:black')
+        self.ax[1].grid(True)
+        self.ax[1].set_xlabel('Time/sec')
+        self.ax[1].set_ylabel('Voltage/V')
+
+        self.fig.tight_layout()
+        self.fig.canvas.draw()
+
+        self.scpi_cmd = {'voltage': [self.amp_val_ch1, self.amp_val_ch2],
+                         'frequency': [self.freq_val_ch1, self.freq_val_ch2],
+                         'phase': [self.phase_val_ch1, self.phase_val_ch2],
+                         'offset': [self.offset_val_ch1, self.offset_val_ch2]}
+
+        # SG().para_set(self.cmd)
 
     def button_switch(self):
         import tkinter as tk
@@ -150,13 +191,20 @@ class MyWindow:
 
         self.window.mainloop()
     def window_geometry(self):
+        for id_row, row in enumerate([1]*7):
+            self.window.grid_rowconfigure(id_row, weight=row)
+        for id_col, col in enumerate([5, 2, 2, 2]):
+            self.window.grid_columnconfigure(id_col, weight=col)
+
         screen_width = self.window.winfo_screenwidth()
         screen_height = self. window.winfo_screenheight()
         self.window_width = screen_width * .4
         self.window_height = screen_height * .2
         self.window_start_x = 100
         self.window_start_y = 100
-        window.geometry("%dx%d+%d+%d" % (self.window_width, self.window_height, self.window_start_x, self.window_start_y))
+        window.geometry("%dx%d+%d+%d" % (self.window_width, self.window_height,
+                                         self.window_start_x,
+                                         self.window_start_y))
 
 window = tk.Tk()
 mywin = MyWindow(window)
