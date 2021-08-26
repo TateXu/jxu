e3b9be46f7be5ab90b93fa72bd1b509dee4cc159
root <jiachen.xu.94@gmail.com>
2021-08-26 16:18:29 +0200

    finish the prototype of remote stimulator

--- hardware/standalone_stimulator/test_gui.py
+++ hardware/standalone_stimulator/test_gui.py
@@ -18,6 +18,8 @@ import tkinter as tk
 import tkinter.font as tkFont
 from tkinter import Label, Entry, Button, OptionMenu, StringVar, END
 
+from PIL import Image, ImageTk
+
 from signal_generator import SignalGenerator as SG
 
 class tACSWindow():
@@ -37,17 +39,48 @@ class tACSWindow():
         self.window.mainloop()
 
     def button_place(self):
+        root = '/home/jxu/anaconda3/lib/python3.7/site-packages/jxu/hardware/standalone_stimulator/'
+        subsps_coef = 2
 
-        self.ch1_label = Label(self.window, text='CH1', font=self.fontStyle)
-        self.ch2_label = Label(self.window, text='CH2', font=self.fontStyle)
+        im_ch1 = tk.PhotoImage(file=f'{root}/Figures/label_ch1.png').subsample(subsps_coef)
+        self.ch1_label = Label(self.window, image=im_ch1, font=self.fontStyle, bg="white")
+        self.ch1_label.image = im_ch1
 
-        self.freq_label = Label(self.window, text='Frequency (Hz)', font=self.fontStyle)
-        self.phase_label = Label(self.window, text='Phase (Degree)', font=self.fontStyle)
-        self.amp_label = Label(self.window, text='Voltage (V)', font=self.fontStyle)
-        self.offset_label = Label(self.window, text='Offset (V)', font=self.fontStyle)
-        self.fade_label = Label(self.window, text='Fade In/Out (s)', font=self.fontStyle)
-        self.duration_label = Label(self.window, text='Stim Duration', font=self.fontStyle)
-        self.output_label = Label(self.window, text='Output', font=self.fontStyle)
+        im_ch2 = tk.PhotoImage(file=f'{root}/Figures/label_ch2.png').subsample(subsps_coef)
+        self.ch2_label = Label(self.window, image=im_ch2, font=self.fontStyle, bg="white")
+        self.ch2_label.image = im_ch2
+
+        im_amp = tk.PhotoImage(file=f'{root}/Figures/label_amp.png').subsample(subsps_coef)
+        self.amp_label = Label(self.window, image=im_amp, font=self.fontStyle, bg="white")
+        self.amp_label.image = im_amp
+
+        im_freq = tk.PhotoImage(file=f'{root}/Figures/label_freq.png').subsample(subsps_coef)
+        self.freq_label = Label(self.window, image=im_freq, font=self.fontStyle, bg="white")
+        self.freq_label.image = im_freq
+
+        im_phase = tk.PhotoImage(file=f'{root}/Figures/label_phase.png').subsample(subsps_coef)
+        self.phase_label = Label(self.window, image=im_phase, font=self.fontStyle, bg="white")
+        self.phase_label.image = im_phase
+
+        im_offset = tk.PhotoImage(file=f'{root}/Figures/label_offset.png').subsample(subsps_coef)
+        self.offset_label = Label(self.window, image=im_offset, font=self.fontStyle, bg="white")
+        self.offset_label.image = im_offset
+
+        im_fade = tk.PhotoImage(file=f'{root}/Figures/label_fade_dur.png').subsample(subsps_coef)
+        self.fade_label = Label(self.window, image=im_fade, font=self.fontStyle, bg="white")
+        self.fade_label.image = im_fade
+
+        im_dur = tk.PhotoImage(file=f'{root}/Figures/label_stim_dur.png').subsample(subsps_coef)
+        self.duration_label = Label(self.window, image=im_dur, font=self.fontStyle, bg="white")
+        self.duration_label.image = im_dur
+
+        im_output = tk.PhotoImage(file=f'{root}/Figures/label_output.png').subsample(subsps_coef)
+        self.output_label = Label(self.window, image=im_output, font=self.fontStyle, bg="white")
+        self.output_label.image = im_output
+
+        im_stim_type = tk.PhotoImage(file=f'{root}/Figures/label_stim_type.png').subsample(subsps_coef)
+        self.stim_label = Label(self.window, image=im_stim_type, font=self.fontStyle, bg="white")
+        self.stim_label.image = im_stim_type
 
         self.freq_entry_ch1 = Entry(self.window, font=self.fontStyle)
         self.phase_entry_ch1 = Entry(self.window, font=self.fontStyle)
@@ -68,6 +101,7 @@ class tACSWindow():
         self.ch1_label.grid(row=0, column=2)
         self.ch2_label.grid(row=0, column=3)
         self.output_label.grid(row=8, column=1)
+        self.stim_label.grid(row=1, column=1)
 
         self.comp_list = np.asarray(
             [[self.amp_label, self.freq_label, self.phase_label,
@@ -89,21 +123,31 @@ class tACSWindow():
 
     def init_checkbox(self):
 
-        self.stim_label = Label(self.window, text='Stimulation Type', font=self.fontStyle)
-        self.stim_label.grid(row=1, column=1)
-
         stim_option = ['tACS', 'tDCS', 'tRNS']
         self.click_ch1 = StringVar()
         self.click_ch1.set(stim_option[0])
         self.stim_menu_ch1 = OptionMenu(self.window, self.click_ch1, *stim_option,
                                         command=self.entry_state_update)
-        self.stim_menu_ch1.grid(row=1, column=2)
+        self.stim_menu_ch1.grid(row=1, column=2, ipadx=90)
+        self.stim_menu_ch1.configure(font=self.fontStyle)
+        self.menu_opt_ch1 = self.window.nametowidget(self.stim_menu_ch1.menuname)
+        self.menu_opt_ch1.config(font=self.fontStyle)
 
         self.click_ch2 = StringVar()
         self.click_ch2.set(stim_option[0])
         self.stim_menu_ch2 = OptionMenu(self.window, self.click_ch2, *stim_option,
                                         command=self.entry_state_update)
-        self.stim_menu_ch2.grid(row=1, column=3)
+        self.stim_menu_ch2.grid(row=1, column=3, ipadx=90)
+        self.stim_menu_ch2.configure(font=self.fontStyle)
+        self.menu_opt_ch2 = self.window.nametowidget(self.stim_menu_ch2.menuname)
+        self.menu_opt_ch2.config(font=self.fontStyle)
+
+
+        # from tkinter import ttk
+        # self.click_ch1 = ttk.Combobox(self.window, values=stim_option,
+                                      # postcommand=self.entry_state_update)
+        # self.click_ch1.grid(row=1, column=2)
+        # self.click_ch1.bind("<<ComboboxSelected>>", self.entry_state_update)
 
     def entry_state_update(self, event):
         for id_click, click in enumerate([self.click_ch1, self.click_ch2]):
@@ -127,7 +171,8 @@ class tACSWindow():
 
     def wave_display(self):
 
-        self.fig, self.ax = plt.subplots(2, 1, facecolor=(1, 1, 1))
+        self.fig, self.ax = plt.subplots(2, 1, facecolor=(1, 1, 1),
+                                         figsize=(self.window_width*0.4/100, self.window_height*0.8/100))
         self.ax[0].set_title('CH1')
         self.ax[0].set_facecolor('xkcd:black')
         self.ax[0].grid(True)
@@ -157,12 +202,25 @@ class tACSWindow():
             self.fig.canvas.draw()
 
         # plot_widget.pack(expand=True, side=LEFT)
-        plot_widget.grid(row=1, column=0, rowspan=9)
-        self.bt_out1 = tk.Button(self.window, text="CH1", command=lambda: self.signal_out(chn=1))
+        plot_widget.grid(row=1, column=0, rowspan=7)
+
+        im_on = './Figures/button_on.png'
+        im_off = "./Figures/button_off.png"
+        self.window.fig_on = tk.PhotoImage(file=im_on).subsample(3)
+        self.window.fig_off = tk.PhotoImage(file=im_off).subsample(3)
+
+        self.bt_out1 = tk.Button(self.window, image=self.window.fig_off, bg="white", command=lambda: self.signal_out(chn=1))
+        self.bt_out1.image = self.window.fig_off
         self.bt_out1.grid(row=8, column=2)
-        self.bt_out2 = tk.Button(self.window, text="CH2", command=lambda: self.signal_out(chn=2))
+        self.bt_out2 = tk.Button(self.window, image=self.window.fig_off, bg="white", command=lambda: self.signal_out(chn=2))
+        self.bt_out2.image = self.window.fig_off
         self.bt_out2.grid(row=8, column=3)
-        tk.Button(self.window, text="Update Parameters", command=self.para_update).grid(row=9, column=1, columnspan=3)
+
+        im_update = "./Figures/button_update.png"
+        self.window.fig_update = tk.PhotoImage(file=im_update).subsample(3)
+        self.bt_update = tk.Button(self.window, image=self.window.fig_update, command=self.para_update)
+        self.bt_update.image = self.window.fig_update
+        self.bt_update.grid(row=8, column=0)
         # self.window.mainloop()
 
     def para_update(self):
@@ -180,12 +238,16 @@ class tACSWindow():
         def state_switch(button, channel):
             if button["state"] == "normal":
                 button["state"] = "active"
-                self.sig_gen.on(chn=channel)
+                if self.dev_available:
+                    self.sig_gen.on(chn=channel)
+                button.configure(image=self.window.fig_on)
                 # vlabel.configure(image=self.window.photo1)
                 print('active')
             else:
                 button["state"] = "normal"
-                self.sig_gen.off(chn=channel)
+                if self.dev_available:
+                    self.sig_gen.off(chn=channel)
+                button.configure(image=self.window.fig_off)
                 # vlabel.configure(image=self.window.photo)
                 print('normal')
 
@@ -223,6 +285,7 @@ class tACSWindow():
         # photo1 = "Figures/button2.jpeg"
         # self.window.photo = ImageTk.PhotoImage(Image.open(photo))
         # self.window.photo0 = ImageTk.PhotoImage(Image.open(photo1))
+
 
         if chn == 1:
             amplitude, stim_dur, fade_dur = self.amp_val_ch1, self.duration_val_ch1, self.fade_val_ch1
@@ -368,36 +431,6 @@ class tACSWindow():
             print(self.scpi_cmd_ch)
             self.sig_gen.para_set(self.scpi_cmd_ch, chn=chn)
 
-
-    def button_switch(self):
-        import tkinter as tk
-        from PIL import Image, ImageTk
-
-        def change_pic():
-            print(b2['state'])
-            if b2["state"] == "normal":
-                # b2["text"] = "enable"
-                b2["state"] = "active"
-                vlabel.configure(image=self.window.photo1)
-                print('active')
-            else:
-                b2["state"] = "normal"
-                b2["text"] = "disable"
-                vlabel.configure(image=self.window.photo)
-                print('normal')
-
-        photo = 'Figures/button1.jpeg'
-        photo1 = "Figures/button2.jpeg"
-        self.window.photo = ImageTk.PhotoImage(Image.open(photo))
-        self.window.photo0 = ImageTk.PhotoImage(Image.open(photo1))
-
-        vlabel=tk.Label(self.window, image=self.window.photo)
-        vlabel.pack()
-
-        b2=tk.Button(window,text="Capture",command=change_pic)
-        b2.pack()
-
-        self.window.mainloop()
     def window_geometry(self):
         for id_row, row in enumerate([1]*7):
             self.window.grid_rowconfigure(id_row, weight=row)
@@ -406,7 +439,7 @@ class tACSWindow():
 
         screen_width = self.window.winfo_screenwidth()
         screen_height = self. window.winfo_screenheight()
-        self.window_width = screen_width * .4
+        self.window_width = screen_width * .3
         self.window_height = screen_height * .2
         self.window_start_x = 100
         self.window_start_y = 100
